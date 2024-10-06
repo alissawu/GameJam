bb2137418adbd99f9ad874f86b4984a0e7704f3f
Judyzc <118636029+Judyzc@users.noreply.github.com>
2024-10-06 03:51:23 -0400

    have panel manager, just need to figure out how to show. tomorrow? :D

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -4,61 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-// public class QuizManager : MonoBehaviour
-// {
-//     public List<QuestionAndAnswers> QnA; // for manual adds, we won't need to worry about it i think 
-//     public GameObject[] options; // UI buttons
-//     // public int currentQuestion;
-//     public TMP_Text QuestionTxt; // question text
-
-//     private Dictionary<string, int> categoryScores = new Dictionary<string, int>(); // scores for each category
-//     public QuestionAndAnswersData questionData; // reference to QuestionsAndAnswersData object
-
-//     private int currentQuestionIndex = 0; // track current question
-//     private int currentStage = 1; // track current stage (start at stage 1)
-//     private List<QuestionAndAnswers> stageQuestions; // questions for the current stage
-
-
-//     private void Start() {
-//         QnA = questionData.QnA; // fills it out with answers
-//         generateQuestion();
-//     }
-
-//     void setAnswers() {
-//         for (int i=0; i<options.Length; i++) { 
-//             if (i < QnA[currentQuestion].Answers.Length) { // Check if there are enough answers
-//                 options[i].transform.GetChild(0).GetComponent<TMP_Text>().text = QnA[currentQuestion].Answers[i];
-//                 options[i].GetComponent<AnswerScript>().category = QnA[currentQuestion].Categories[i]; // Set the associated category
-//                 options[i].SetActive(true); // Make sure the button is active
-//             } else {
-//                 options[i].SetActive(false); // Hide any unused options
-//             }
-
-//             options[i].transform.GetChild(0).GetComponent<TMP_Text>().text = QnA[currentQuestion].Answers[i];
-//             options[i].GetComponent<AnswerScript>().category = QnA[currentQuestion].Categories[i];
-//         }
-//     }
-
-//     void generateQuestion() {
-//         if (QnA.Count > 0) {
-//             currentQuestion = Random.Range(0, QnA.Count); // Select a random question from the list
-//             QuestionTxt.text = QnA[currentQuestion].Question; // Update the question text
-//             setAnswers(); // Update the buttons with new answers and categories
-//         } else {
-//             Debug.Log("Out of Questions");
-//         }
-//     }
-
-//     public void collectCategory(string category) { 
-//         if (categoryScores.ContainsKey(category)) {
-//             categoryScores[category]++;
-//         } else {
-//             categoryScores[category] = 1;
-//         }
-//         Debug.Log($"Category {category} score: {categoryScores[category]}");
-//     }
-// }
-
 public class QuizManager : MonoBehaviour
 {
     public QuestionAndAnswersData questionData; // Reference to the ScriptableObject
@@ -70,13 +15,17 @@ public class QuizManager : MonoBehaviour
 
     private Dictionary<string, int> categoryScores = new Dictionary<string, int>(); // Keep track of category scores
 
+    public PanelImageManager panelImageManager;
+
     private void Start() {
+        panelImageManager.SetPanelImage(currentStage); // Set the initial image
         generateStage(); // Start by generating the first stage
     }
 
     void generateStage() {
         if (currentStage < questionData.stages.Count) {
             currentQuestionInStage = 0; // Start at the first question of the new stage
+            panelImageManager.SetPanelImage(currentStage); // Update image when stage changes
             generateQuestion();
         } else {
             Debug.Log("All stages completed.");
@@ -118,7 +67,6 @@ public class QuizManager : MonoBehaviour
         }
 
         Debug.Log($"Category {category} score: {categoryScores[category]}");
-
         // Move to the next question in the stage after selecting an answer
         currentQuestionInStage++;
         generateQuestion(); // Generate the next question
